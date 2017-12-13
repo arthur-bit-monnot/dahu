851f929fbb23ff66c6f0c7cae4b0e4a2797005b5
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-12-13 18:45:36 +0100

    Added toString methods to Algebra

--- src/main/scala/dahu/recursion/ResultF.scala
+++ src/main/scala/dahu/recursion/ResultF.scala
@@ -16,13 +16,12 @@ sealed abstract class ResultF[F] {
   def typ: TT
 }
 
-case class InputF[F](name: String, typ: TT)                           extends ResultF[F]
-case class CstF[F](value: Top, typ: TT)                               extends ResultF[F]
-final case class ComputationF[F](fun: Fun[_], args: List[F], typ: TT) extends ResultF[F]
-
-object Test {
-
-  import matryoshka._
-  import matryoshka.data._
-
+case class InputF[F](name: String, typ: TT)                           extends ResultF[F] {
+  override def toString: String = s"?$name"
+}
+case class CstF[F](value: Top, typ: TT)                               extends ResultF[F] {
+  override def toString: String = value.toString
+}
+final case class ComputationF[F](fun: Fun[_], args: List[F], typ: TT) extends ResultF[F] {
+  override def toString: String = s"λ: $fun(${args.mkString(", ")})"
 }
