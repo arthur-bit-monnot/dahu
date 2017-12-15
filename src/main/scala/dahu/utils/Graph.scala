8c086d597aaccbb8d53f7ee25271eb91f432a432
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-12-15 17:15:10 +0100

    Added graph utils for strongly connected components and topological orders.

---
+++ src/main/scala/dahu/utils/Graph.scala
@@ -0,0 +1,64 @@
+package dahu.utils
+
+import scala.collection.{immutable, mutable}
+
+object Graph {
+
+  /** Tarjan's algorithm for extracting strongly connected components.
+    *
+    * Given a directed graph, the algorithm outputs a sequence of strongly connected
+    * components sorted in topological order.
+    * */
+  def tarjan[V](graph: Map[V, Set[V]]): Seq[Set[V]] = {
+    class Data(var index: Int, var lowLink: Int, var onStack: Boolean)
+    var index = 0
+    val stack     = new mutable.ArrayBuffer[V]()
+    val data  = mutable.Map[V, Data]()
+
+    var components: Seq[Set[V]] = immutable.Seq()
+
+    for (v <- graph.keys) {
+      if (!data.contains(v))
+        strongConnect(v)
+    }
+
+    def strongConnect(v: V): Unit = {
+      assert(!data.contains(v))
+      data(v) = new Data(index, index, true)
+      index += 1
+      stack += v
+      for (w <- graph.getOrElse(v, Set())) {
+        if (!data.contains(w)) {
+          strongConnect(w)
+          data(v).lowLink = data(v).lowLink.min(data(w).lowLink)
+        } else if (data(w).onStack) {
+          data(v).lowLink = data(v).lowLink.min(data(w).index)
+        }
+      }
+
+      if (data(v).lowLink == data(v).index) {
+        var scc: Set[V] = Set()
+        var w           = stack.last
+        do {
+          w = stack.last
+          stack.remove(stack.size - 1)
+          data(w).onStack = false
+          scc += w
+        } while (w != v)
+        components = components :+ scc
+      }
+    }
+
+    components.reverse
+  }
+
+  def topologicalOrder[V](graph: Map[V, Set[V]]): Option[Seq[V]] = {
+    val sccs = tarjan(graph)
+
+    sccs.foldLeft(Option(Seq[V]())){
+      case (opt, scc) if scc.size == 1 => opt.map(_ :+ scc.head)
+      case _ => None // non singleton strongly connected component
+    }
+  }
+
+}
---
+++ src/test/scala/dahu/utils/UtilsTests.scala
@@ -0,0 +1,30 @@
+package dahu.utils
+
+import org.scalatest.FreeSpec
+import Graph._
+
+class UtilsTests extends FreeSpec {
+
+  // graph with 2 SCC
+  val g1: Map[Int, Set[Int]] = Map(1 -> Set(2,3), 2 -> Set(1))
+
+  // directed acyclic graphs
+  val dag1: Map[Int, Set[Int]] = Map(1 -> Set(2), 2 -> Set(3), 3 -> Set(4))
+  val dag2: Map[Int, Set[Int]] = Map(1 -> Set(2, 4), 2 -> Set(3, 4), 3 -> Set(4))
+
+  "graph-utils" - {
+    "tarjan" in {
+
+      assert(tarjan(g1) == Seq(Set(1, 2), Set(3)))
+      assert(tarjan(dag1) == (1 to 4).map(Set(_)))
+      assert(tarjan(dag2) == (1 to 4).map(Set(_)))
+
+    }
+    "topological-order" in {
+      assert(topologicalOrder(g1).isEmpty)
+      assert(topologicalOrder(dag1).contains(1 to 4))
+      assert(topologicalOrder(dag2).contains(1 to 4))
+    }
+  }
+
+}
