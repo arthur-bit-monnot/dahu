aea881fe77b80fcc701e4a6af389643a16119918
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2018-04-06 12:06:47 +0200

    Clean up on the full2core compilation.

--- copla-lang/src/main/scala/copla/lang/model/transforms/FullToCore.scala
+++ copla-lang/src/main/scala/copla/lang/model/transforms/FullToCore.scala
@@ -3,120 +3,97 @@ package copla.lang.model.transforms
 import copla.lang.model
 import copla.lang.model._
 import copla.lang.model.full.Scope
-import landscaper.transformations.Trans
-import shapeless.Poly1
 
 object FullToCore {
 
   case class Context(scope: Scope, config: Config = Config())
 
-  private def staticExprToVar(expr: full.StaticExpr)(
-      implicit ctx: Context): (core.Var, Seq[core.Statement]) = {
-    expr match {
-      case x: core.Var => (x, Seq())
-      case x: full.Constant =>
-        val (params, statements) = staticExprsToVars(x.params)
-        val cst = core.Constant(x.template, params)
-        val variable = core.LocalVar(ctx.scope.makeNewId(), cst.typ)
-        val declaration = core.LocalVarDeclaration(variable)
-        val eq = core.BindAssertion(cst, variable)
-        (variable, statements :+ declaration :+ eq)
+  /** Monad that represent an expression of type A that is subject to the associated statements to restrict its values. */
+  private case class CoreM[+A](value: A, statements: Seq[core.Statement]) {
+    def flatMap[B](f: A => CoreM[B]): CoreM[B] = {
+      val fb = f(value)
+      CoreM(fb.value, statements ++ fb.statements)
     }
+    def map[B](f: A => B): CoreM[B] = CoreM(f(value), statements)
   }
-  private def staticExprToVarTRANSM(expr: full.StaticExpr)(
-      implicit ctx: Context): TransM[core.Var] = {
+  private object CoreM {
+    def pure[A](a: A): CoreM[A] = CoreM(a, Seq())
+    def unit(statements: core.Statement*): CoreM[Unit] = CoreM((), statements)
+  }
+
+  private def f2c(expr: full.StaticExpr)(implicit ctx: Context): CoreM[core.Var] = {
     expr match {
-      case x: core.Var => TransM.pure(x)
+      case x: core.Var => CoreM.pure(x)
       case x: full.Constant =>
-        val (params, statements) = staticExprsToVars(x.params)
-        val cst = core.Constant(x.template, params)
-        val variable = core.LocalVar(ctx.scope.makeNewId(), cst.typ)
-        val declaration = core.LocalVarDeclaration(variable)
-        val eq = core.BindAssertion(cst, variable)
-        TransM(variable, statements :+ declaration :+ eq)
+        for {
+          params <- f2c(x.params.toList)
+          cst = core.Constant(x.template, params)
+          variable = core.LocalVar(ctx.scope.makeNewId(), cst.typ)
+          _ <- CoreM.unit(core.LocalVarDeclaration(variable))
+          _ <- CoreM.unit(core.BindAssertion(cst, variable))
+        } yield variable
     }
   }
 
-  case class TransM[A](a: A, accumulatedStatement: Seq[core.Statement]) {
-    def flatMap[B](f: A => TransM[B]): TransM[B] = {
-      val ffb = f(a)
-      TransM(ffb.a, accumulatedStatement ++ ffb.accumulatedStatement)
-    }
-    def map[B](f: A => B): TransM[B] = TransM(f(a), accumulatedStatement)
-  }
-  object TransM {
-    def pure[A](a: A): TransM[A] = TransM(a, Seq())
-    def unit(statements: core.Statement*): TransM[Unit] = TransM((), statements)
-  }
-
-  private def fullIntExprToCore(e: full.IntExpr)(implicit ctx: Context): TransM[core.IntExpr] =
+  private def f2c(e: full.IntExpr)(implicit ctx: Context): CoreM[core.IntExpr] =
     e match {
-      case x: core.IntExpr => TransM.pure(x)
+      case x: core.IntExpr => CoreM.pure(x)
       case full.GenIntExpr(e) =>
         for {
-          v <- staticExprToVarTRANSM(e)
+          v <- f2c(e)
         } yield core.VarIntExpr(v)
+
       case full.Minus(e) =>
         for {
-          ce <- fullIntExprToCore(e)
+          ce <- f2c(e)
         } yield core.Minus(ce)
 
       case full.Add(lhs, rhs) =>
         for {
-          el <- fullIntExprToCore(lhs)
-          er <- fullIntExprToCore(rhs)
+          el <- f2c(lhs)
+          er <- f2c(rhs)
         } yield core.Add(el, er)
     }
 
-  private def fullToCoreTimepoint(tp: full.TPRef)(implicit ctx: Context): TransM[core.TPRef] =
+  private def f2c(tp: full.TPRef)(implicit ctx: Context): CoreM[core.TPRef] =
     tp match {
-      case x: core.TPRef => TransM.pure(x)
+      case x: core.TPRef => CoreM.pure(x)
       case x: full.TPRef =>
         for {
-          de <- fullIntExprToCore(x.delay)
+          de <- f2c(x.delay)
         } yield core.TPRef(x.id, de)
     }
 
-  private def staticExprsToVars(exprs: Seq[full.StaticExpr])(
-      implicit ctx: Context): (Seq[core.Var], Seq[core.Statement]) =
-    exprs
-      .map(param => staticExprToVar(param))
-      .foldLeft((Seq[core.Var](), Seq[core.Statement]())) {
-        case ((params, statements), (param, additionalStmts)) =>
-          (params :+ param, statements ++ additionalStmts)
-      }
-  private def staticExprsToVarsTRANSM(exprs: List[full.StaticExpr])(
-      implicit ctx: Context): TransM[List[core.Var]] = {
+  private def f2c(exprs: List[full.StaticExpr])(implicit ctx: Context): CoreM[List[core.Var]] = {
     exprs match {
-      case Nil => TransM.pure(Nil)
+      case Nil => CoreM.pure(Nil)
       case head :: tail =>
         for {
-          h <- staticExprToVarTRANSM(head)
-          t <- staticExprsToVarsTRANSM(tail)
+          h <- f2c(head)
+          t <- f2c(tail)
         } yield h :: t
     }
   }
 
-  def timedSymExpr2CoreFluent(expr: full.TimedSymExpr)(
-      implicit ctx: Context): TransM[core.Fluent] = {
+  private def f2c(expr: full.TimedSymExpr)(implicit ctx: Context): CoreM[core.Fluent] = {
     expr match {
       case fluent: full.Fluent =>
         for {
-          params <- staticExprsToVarsTRANSM(fluent.params.toList)
+          params <- f2c(fluent.params.toList)
         } yield core.Fluent(fluent.template, params)
     }
   }
 
-  def trans(act: full.ActionTemplate)(implicit ctx: Context): core.ActionTemplate = {
+  private def f2c(act: full.ActionTemplate)(implicit ctx: Context): core.ActionTemplate = {
     val statements = act.store.blocks.flatMap {
       case x: core.Statement => Seq(x)
-      case x: full.Statement => trans(x).accumulatedStatement
+      case x: full.Statement => f2c(x).statements
     }
     core.ActionTemplate(act.scope, statements)
   }
 
-  def trans(block: full.Statement)(implicit ctx: Context): TransM[Unit] = block match {
-    case x: core.Statement => TransM.unit(x)
+  private def f2c(block: full.Statement)(implicit ctx: Context): CoreM[Unit] = block match {
+    case x: core.Statement => CoreM.unit(x)
 
     case x: full.StaticAssignmentAssertion =>
       (x.left, x.right) match {
@@ -124,7 +101,7 @@ object FullToCore {
             if cst.params.forall(_.isInstanceOf[core.Term]) =>
           val boundCst =
             new model.core.BoundConstant(cst.template, cst.params.map(_.asInstanceOf[core.Term]))
-          TransM.unit(core.StaticAssignmentAssertion(boundCst, inst))
+          CoreM.unit(core.StaticAssignmentAssertion(boundCst, inst))
         case _ =>
           throw new UnsupportedOperationException(
             s"Assignment assertions on constant functions are only supported when all parameters are declared instances: $block")
@@ -132,149 +109,99 @@ object FullToCore {
 
     case x: full.StaticEqualAssertion =>
       for {
-        lVar <- staticExprToVarTRANSM(x.left)
-        rVar <- staticExprToVarTRANSM(x.right)
-        _ <- TransM.unit(core.StaticEqualAssertion(lVar, rVar))
+        lVar <- f2c(x.left)
+        rVar <- f2c(x.right)
+        _ <- CoreM.unit(core.StaticEqualAssertion(lVar, rVar))
       } yield ()
-//      val (lVar, lStmts) = staticExprToVar(x.left)
-//      val (rVar, rStmts) = staticExprToVar(x.right)
-//      lStmts ++ rStmts :+ core.StaticEqualAssertion(lVar, rVar)
 
     case x: full.StaticDifferentAssertion =>
       for {
-        lVar <- staticExprToVarTRANSM(x.left)
-        rVar <- staticExprToVarTRANSM(x.right)
-        _ <- TransM.unit(core.StaticDifferentAssertion(lVar, rVar))
+        lVar <- f2c(x.left)
+        rVar <- f2c(x.right)
+        _ <- CoreM.unit(core.StaticDifferentAssertion(lVar, rVar))
       } yield ()
 
     case full.TemporallyQualifiedAssertion(qualifier, assertion) =>
-//      val (start, end, baseStatements: Seq[core.Statement]) =
-//        qualifier match {
-//          case full.Equals(interval) =>
-//            if(ctx.config.mergeTimepoints && assertion.name.startsWith(reservedPrefix)) {
-//              // we are asked to merge timepoints and assertion was not given a name
-//              // use the timepoints from the interval instead of the one of the assertion
-//              (interval.start, interval.end, Seq())
-//            } else {
-//              (assertion.start,
-//               assertion.end,
-//               Seq(
-//                 Seq(core.TimepointDeclaration(assertion.start)),
-//                 Seq(core.TimepointDeclaration(assertion.end)),
-//                 interval.start === assertion.start,
-//                 interval.end === assertion.end
-//               ).flatten)
-//            }
-//          case full.Contains(interval) =>
-//            (assertion.start,
-//             assertion.end,
-//             Seq(
-//               core.TimepointDeclaration(assertion.start),
-//               core.TimepointDeclaration(assertion.end),
-//               interval.start <= assertion.start,
-//               assertion.end <= interval.end
-//             ))
-//        }
-      val startEnd: TransM[(core.TPRef, core.TPRef)] =
+      val startEnd: CoreM[(core.TPRef, core.TPRef)] =
         qualifier match {
+          case full.Equals(interval)
+              if ctx.config.mergeTimepoints && assertion.name.startsWith(reservedPrefix) =>
+            // we are asked to merge timepoints and assertion was not given a name
+            // use the timepoints from the interval instead of the one of the assertion
+            for {
+              start <- f2c(interval.start)
+              end <- f2c(interval.end)
+            } yield (start, end)
           case full.Equals(interval) =>
-            if(ctx.config.mergeTimepoints && assertion.name.startsWith(reservedPrefix)) {
-              // we are asked to merge timepoints and assertion was not given a name
-              // use the timepoints from the interval instead of the one of the assertion
-              for {
-                start <- fullToCoreTimepoint(interval.start)
-                end <- fullToCoreTimepoint(interval.end)
-              } yield (start, end)
-            } else {
-              ???
-//              (assertion.start,
-//               assertion.end,
-//               Seq(
-//                 Seq(core.TimepointDeclaration(assertion.start)),
-//                 Seq(core.TimepointDeclaration(assertion.end)),
-//                 interval.start === assertion.start,
-//                 interval.end === assertion.end
-//               ).flatten)
-            }
+            for {
+              itvStart <- f2c(interval.start)
+              itvEnd <- f2c(interval.end)
+              _ <- CoreM.unit(core.TimepointDeclaration(assertion.start),
+                              core.TimepointDeclaration(assertion.end))
+              _ <- CoreM.unit(itvStart === assertion.start: _*)
+              _ <- CoreM.unit(assertion.end === itvEnd: _*)
+
+            } yield (assertion.start, assertion.end)
           case full.Contains(interval) =>
             for {
-              itvStart <- fullToCoreTimepoint(interval.start)
-              itvEnd <- fullToCoreTimepoint(interval.end)
-              _ <- TransM.unit(
+              itvStart <- f2c(interval.start)
+              itvEnd <- f2c(interval.end)
+              _ <- CoreM.unit(
                 core.TimepointDeclaration(assertion.start),
                 core.TimepointDeclaration(assertion.end),
                 itvStart <= assertion.start,
                 assertion.end <= itvEnd
               )
             } yield (assertion.start, assertion.end)
-//            (assertion.start,
-//             assertion.end,
-//             Seq(
-//               core.TimepointDeclaration(assertion.start),
-//               core.TimepointDeclaration(assertion.end),
-//               interval.start <= assertion.start,
-//               assertion.end <= interval.end
-//             ))
         }
       assertion match {
-        case full.TimedEqualAssertion(fluent, value, parent, name) =>
+        case full.TimedEqualAssertion(fluent, value, _, _) =>
           for {
             se <- startEnd
             (start, end) = se
-            coreFluent <- timedSymExpr2CoreFluent(fluent)
-            coreValue <- staticExprToVarTRANSM(value)
-            _ <- TransM.unit(
-              core.TimedEqualAssertion(start, end, coreFluent, coreValue),
-              start <= end
-            )
+            coreFluent <- f2c(fluent)
+            coreValue <- f2c(value)
+            _ <- CoreM.unit(core.TimedEqualAssertion(start, end, coreFluent, coreValue),
+                            start <= end)
           } yield ()
-//          val (coreFluent, fluentStatement) = timedSymExpr2CoreFluent(fluent)
-//          val (coreValue, valueStatements) = staticExprToVar(value)
-//          baseStatements ++ fluentStatement ++ valueStatements :+ core
-//            .TimedEqualAssertion(start, end, coreFluent, coreValue) :+
-//            (start <= end)
-        case full.TimedAssignmentAssertion(fluent, value, parent, name) =>
+
+        case full.TimedAssignmentAssertion(fluent, value, _, _) =>
           for {
             se <- startEnd
             (start, end) = se
-            coreFluent <- timedSymExpr2CoreFluent(fluent)
-            coreValue <- staticExprToVarTRANSM(value)
-            _ <- TransM.unit(
-              core.TimedAssignmentAssertion(start, end, coreFluent, coreValue),
-              start <= end
-            )
+            coreFluent <- f2c(fluent)
+            coreValue <- f2c(value)
+            _ <- CoreM.unit(core.TimedAssignmentAssertion(start, end, coreFluent, coreValue),
+                            start <= end)
           } yield ()
-//          val (coreFluent, fluentStatement) = timedSymExpr2CoreFluent(fluent)
-//          val (coreValue, valueStatements) = staticExprToVar(value)
-//          baseStatements ++ fluentStatement ++ valueStatements :+ core
-//            .TimedAssignmentAssertion(start, end, coreFluent, coreValue) :+
-//            (start <= end)
-        case full.TimedTransitionAssertion(fluent, fromValue, toValue, parent, name) =>
+
+        case full.TimedTransitionAssertion(fluent, fromValue, toValue, _, _) =>
           for {
             se <- startEnd
             (start, end) = se
-            coreFluent <- timedSymExpr2CoreFluent(fluent)
-            coreFrom <- staticExprToVarTRANSM(fromValue)
-            coreTo <- staticExprToVarTRANSM(toValue)
-            _ <- TransM.unit(
-              core.TimedTransitionAssertion(start, end, coreFluent, coreFrom, coreTo),
-              start < end
-            )
+            coreFluent <- f2c(fluent)
+            coreFrom <- f2c(fromValue)
+            coreTo <- f2c(toValue)
+            _ <- CoreM.unit(core.TimedTransitionAssertion(start, end, coreFluent, coreFrom, coreTo),
+                            start < end)
           } yield ()
-//          val (coreFluent, fluentStatement) = timedSymExpr2CoreFluent(fluent)
-//          val (coreFromValue, fromValueStatements) = staticExprToVar(fromValue)
-//          val (coreToValue, toValueStatements) = staticExprToVar(toValue)
-//          baseStatements ++ fluentStatement ++ fromValueStatements ++ toValueStatements :+ core
-//            .TimedTransitionAssertion(start, end, coreFluent, coreFromValue, coreToValue) :+
-//            (start < end)
       }
+
+    case full.TBefore(from, to) =>
+      for {
+        f <- f2c(from)
+        t <- f2c(to)
+        _ <- CoreM.unit(
+          core.TBefore(f, t)
+        )
+      } yield ()
   }
 
   def trans(model: full.Model, config: Config = Config()): Seq[core.InModuleBlock] = {
     model.store.blocks.flatMap {
       case x: core.InModuleBlock  => Seq(x)
-      case x: full.Statement      => trans(x)(Context(model.scope, config)).accumulatedStatement
-      case x: full.ActionTemplate => Seq(trans(x)(Context(model.scope, config)))
+      case x: full.Statement      => f2c(x)(Context(model.scope, config)).statements
+      case x: full.ActionTemplate => Seq(f2c(x)(Context(model.scope, config)))
     }
   }
 
