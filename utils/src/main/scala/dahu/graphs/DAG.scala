ffb4c251b2450b55c77dd8d55c622b0b1e3db684
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2018-06-21 08:18:01 +0200

    DAG is now based on a foreach method.

--- utils/src/main/scala/dahu/graphs/DAG.scala
+++ utils/src/main/scala/dahu/graphs/DAG.scala
@@ -2,6 +2,7 @@ package dahu.graphs
 
 import dahu.utils.Graph
 
+import scala.collection.mutable
 import scala.reflect.ClassTag
 
 /** A Directed Acyclic Graph.
@@ -12,12 +13,32 @@ import scala.reflect.ClassTag
 trait DAG[F[_], A] {
 
   /** Generation of a node from a node ID. */
-  def algebra: A => F[A]
+  def algebra(a: A): F[A]
+
+  def foreachChild(fa: F[A])(f: A => Unit): Unit
 
   /** All direct children of a node. */
-  def children(graph: F[A]): Iterable[A]
+  def children(graph: F[A]): Iterable[A] = {
+    val builder = Iterable.newBuilder[A]
+    foreachChild(graph)(builder += _)
+    builder.result()
+  }
 
-  def descendantsAndSelf(a: A): Set[A] = children(algebra(a)).toSet.flatMap(descendantsAndSelf) + a
+  def descendantsAndSelf(a: A): Set[A] = {
+    val queue = mutable.ArrayBuffer[A]()
+    queue += a
+    val result = mutable.Set[A]()
+    while(queue.nonEmpty) {
+      val cur = queue.remove(queue.length - 1)
+      if(!result.contains(cur))
+        foreachChild(algebra(cur)) { child =>
+          if(!result.contains(child))
+            queue += child
+        }
+      result += cur
+    }
+    result.toSet
+  }
 
   def topologicalOrder(nodes: Set[A])(implicit ct: ClassTag[A]): Seq[A] = {
     val graph = nodes.map(n => (n, children(algebra(n)).toSet)).toMap
